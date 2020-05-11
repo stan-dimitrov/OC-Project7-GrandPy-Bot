ab7942b5d315cc0b873b41a71a9d7954b4321e44
stan-dimitrov <45197828+stan-dimitrov@users.noreply.github.com>
2020-05-11 11:03:45 +0200

    Add files via upload

--- tests.py
+++ tests.py
@@ -2,11 +2,14 @@ import unittest
 import main
 from unittest import TestCase
 from unittest.mock import patch
+import parsing as script
+import pytest
 
+result = main.select_wiki_article(57644)
 class TestMain(unittest.TestCase):
     def test_select_wiki_article(self):
         response = main.select_wiki_article(page=57644)
-        self.assertTrue(response, dict)
+        self.assertTrue(response, result)
 
     def test_search_wiki_articles(self):
         main.search_wiki_articles('Sofia')
@@ -28,6 +31,33 @@ class TestMain(unittest.TestCase):
         response = Random_page.posts()
         self.assertIsNotNone(response)
         self.assertIsInstance(response[0], dict)
+
+
+class TestParser:
+
+    user_query = script.Pars('Bonjour GrandPy! Tu peux me donner l\'adresse du Louvre?')
+
+    def test_get_text(self):
+        # input in lowercase
+        print(self.user_query.textinput)
+        assert self.user_query.textinput == 'bonjour grandpy! tu peux me donner l\'adresse du louvre?'
+
+
+    def test_delete_input_of_symbols(self):
+        #testing the method that delete all Alpha symbols
+        self.user_query.delete_input_of_symbols()
+        print(self.user_query.textinput)
+        assert self.user_query.textinput == 'bonjour grandpy tu peux me donner ladresse du louvre'
+
+    def test_delete_input_of_stopwords(self):
+        #testing themethod that removes all stopwords
+        # self.test_delete_input_of_symbols()
+        self.user_query.delete_input_of_stopwords()
+        assert self.user_query.final_query_in_string != 'bonjour'
+
+    def test_textinput_type(self):
+        # cheking the tipy of our input
+        assert isinstance(self.user_query.textinput, str)
 
 if __name__== "__main__":
     unittest.main()
