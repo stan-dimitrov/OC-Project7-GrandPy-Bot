cf9e063a0409785c4c207ba674be68a88341e083
stan-dimitrov <45197828+stan-dimitrov@users.noreply.github.com>
2020-05-14 09:55:06 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,28 @@
+GOOGLE_MAPS_API = {
+    'URL': 'https://www.google.com/maps/embed/v1/place',
+    'KEY': 'AIzaSyC_foFzuiFp27RE5WfBsOAGFa8vgBOmWKE'  # YOUR_GOOGLE_API_KEY
+}
+
+WIKI_API = {
+    'ARTICLE_URL': 'https://en.wikipedia.org/wiki/',
+    'SEARCH_REQUEST': {
+        'URL': 'https://en.wikipedia.org/w/api.php',
+        'PARAMS': {
+            'action': 'query',
+            'format': 'json',
+            'list': 'search'
+        }
+    },
+    'SELECT_REQUEST': {
+        'URL': 'https://en.wikipedia.org/w/api.php',
+        'PARAMS': {
+            'action': 'query',
+            'format': 'json',
+            'prop': 'extracts',
+            'exlimit': 1,
+            'explaintext': True,
+            'exsectionformat': 'plain',
+            'exintro': True
+        }
+    }
+}
